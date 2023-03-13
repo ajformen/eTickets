ba35a8b8c1541b0a78734cfa73b703932207e9ef
AJ <dev.ajformen@gmail.com>
2023-03-13 15:53:57 +0800

    updated producer

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(buil
 
 // Services configuration
 builder.Services.AddScoped<IActorsService, ActorsService>();
+builder.Services.AddScoped<IProducersService, ProducersService>();
 
 var app = builder.Build();
 
