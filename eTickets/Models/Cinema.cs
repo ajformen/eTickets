eb104b187b1087071528fefe32ad1a29c9fc1243
AJ <dev.ajformen@gmail.com>
2023-03-13 16:59:24 +0800

    updated cinema

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -1,23 +1,28 @@
-﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+﻿using eTickets.Data.Base;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
-    public class Cinema
+    public class Cinema : IEntityBase
     {
         [Key]
         public int Id { get; set; }
 
         [Display(Name = "Cinema logo")]
+        [Required(ErrorMessage = "Cinema logo is required")]
         public string Logo { get; set; }
 
         [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Cinema Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Cinema Name must be between 3 and 50 chars")]
         public string Name { get; set; }
 
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
         // Relationships
-        public List<Movie> Movies { get; set; }
+        public List<Movie>? Movies { get; set; }
     }
 }
